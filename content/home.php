85629a4995fe332562a2bf8027ad015017af4529
kingavin <craftgavin@gmail.com>
2015-05-18 10:18:30 +0800

    a

--- content/home.php
+++ content/home.php
@@ -2,6 +2,7 @@
 	<div id='maximage-holder' style='width: 100%; height: 100%;'>
 		<div id="maximage" style='z-index: 99;'>
 			<img src="<?php echo FILE_PATH?>/h/ss2015-1.jpg" alt="" />
+			<img src="<?php echo FILE_PATH?>/h/ss2015-1.jpg" alt="" />
 		</div>
 	</div>
 </div>
